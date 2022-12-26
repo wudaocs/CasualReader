0f25b9ec44463439da44be146164584c0bd64a59
yue wang <wy08mail@gmail.com>
2022-12-26 15:25:17 +0800

    调整配置

--- app/src/main/java/com/ltd_tech/casualreader/MainActivity.kt
+++ app/src/main/java/com/ltd_tech/casualreader/MainActivity.kt
@@ -2,10 +2,12 @@ package com.ltd_tech.casualreader
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.ltd_tech.core.F
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
