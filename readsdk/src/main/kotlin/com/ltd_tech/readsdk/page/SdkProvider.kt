50f57e7fe56f055908294708f19535d564a6f139
yue wang <wy08mail@gmail.com>
2023-05-21 20:45:46 +0800

    支持epub

--- readsdk/src/main/kotlin/com/ltd_tech/readsdk/page/SdkProvider.kt
+++ readsdk/src/main/kotlin/com/ltd_tech/readsdk/page/SdkProvider.kt
@@ -3,9 +3,13 @@ package com.ltd_tech.readsdk.page
 import android.app.Activity
 import android.content.Context
 import android.content.Intent
+import android.database.Cursor
 import android.net.Uri
+import android.provider.MediaStore
+import androidx.core.content.FileProvider
 import com.ltd_tech.core.exts.globalToast
 import com.ltd_tech.core.utils.CRFileType
+import com.ltd_tech.core.utils.L
 import com.ltd_tech.core.utils.appPackageName
 import com.ltd_tech.readsdk.consts.KEY_EXTRA_BOOK
 import com.ltd_tech.readsdk.consts.KEY_EXTRA_BOOK_IS_LOCAL
@@ -44,7 +48,14 @@ fun jumpToRead(context: Context, bookEntity: BookEntity?) {
         }
 
         CRFileType.EPUB.value -> {
-            muPdf(context, bookEntity.cover)
+            val epubFile = File(bookEntity.cover)
+            val intent = Intent(context, ReadPdfOrEpubActivity::class.java)
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_DOCUMENT)
+            intent.addFlags(Intent.FLAG_ACTIVITY_MULTIPLE_TASK)
+            intent.action = Intent.ACTION_VIEW
+            intent.setDataAndType(FileProvider.getUriForFile(context, "${context.packageName}.fileprovider", epubFile), null)
+            intent.putExtra("$appPackageName.ReturnToLibraryActivity", 1)
+            context.startActivity(intent)
         }
 
         else -> {
@@ -61,6 +72,19 @@ private fun muPdf(context: Context, path: String) {
     intent.setDataAndType(Uri.fromFile(File(path)), null)
     intent.putExtra("$appPackageName.ReturnToLibraryActivity", 1)
     context.startActivity(intent)
+}
+
+fun queryEpubFile(context: Context,epubFilePath: String): Cursor? {
+    // 定义查询的 Uri 和查询所需的列
+    val uri = MediaStore.Files.getContentUri("external")
+    val projection = arrayOf(MediaStore.Files.FileColumns._ID, MediaStore.Files.FileColumns.DISPLAY_NAME)
+
+    // 定义查询条件
+    val selection = "${MediaStore.Files.FileColumns.DATA} = ?"
+    val selectionArgs = arrayOf(epubFilePath)
+
+    // 执行查询并返回 Cursor
+    return context.contentResolver.query(uri, projection, selection, selectionArgs, null)
 }
 
 /**
